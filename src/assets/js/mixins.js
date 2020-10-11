463ccd0f6a6b483635dd3f7c264d4eae52af8e0d
Mack Chen <wind83410@gmail.com>
2020-10-11 15:13:57 +0800

    去除生成頁面路徑 (breadcrumb) 的 linkMap 函數、修改路徑中英對照

--- src/assets/js/mixins.js
+++ src/assets/js/mixins.js
@@ -1,83 +1,33 @@
-export const twEngTable = {
+export const signs = {
   data () {
     return {
-      twEngTable: {
-        className: [
-          {
-            zhTw: '貓主食',
-            eng: 'staple_food'
-          },
-          {
-            zhTw: '貓零食',
-            eng: 'treats'
-          }
-        ],
-        typeName: [
-          {
-            zhTw: '冷凍乾燥主食餐',
-            eng: 'freeze-dried'
-          },
-          {
-            zhTw: '貓飼料/乾糧',
-            eng: 'dry_food'
-          },
-          {
-            zhTw: '貓罐頭/餐盒',
-            eng: 'wet_food'
-          },
-          {
-            zhTw: '肉泥',
-            eng: 'lickable_treats'
-          },
-          {
-            zhTw: '夾心餅',
-            eng: 'rolls'
-          }
-        ]
-      }
-    }
-  },
-  methods: {
-    linkMap (outputMode, className, type = 'all') {
-      const allModes = ['route', 'list', 'breadcrumb']
-      if (!allModes.some(el => el === outputMode)) {
-        throw Error('Output mode error: should be "route," "list" or "breadcrumb"')
-      }
-      const inputMode = outputMode === 'route' ? 'zhTw' : 'eng'
-      const theClass = this.twEngTable.className.find(function (el) {
-        return el[inputMode] === className
-      })
-      const theType = type === 'all' ? type : this.twEngTable.typeName.find(function (el) {
-        return el[inputMode] === type
-      })
-      switch (outputMode) {
-        case 'route':
-          return `${theClass.eng}/${theType === 'all' ? theType : theType.eng}`
-        case 'list': {
-          const obj = {}
-          obj.className = theClass.zhTw
-          obj.typeName = theType === 'all' ? theType : theType.zhTw
-          return obj
+      signs: [
+        {
+          category: '貓主食',
+          route: 'staple-food',
+          description: '乾飼料、罐頭、冷凍主食餐'
+        },
+        {
+          category: '貓零食',
+          route: 'treats',
+          description: '肉泥、餅乾'
+        },
+        {
+          category: '起居用品',
+          route: 'daily-life-utilities',
+          description: '貓砂便盆、外出籠、除毛用品、餐具飲水、居家清潔'
+        },
+        {
+          category: '玩具',
+          route: 'toys',
+          description: '逗貓棒、貓抓板製品、貓草魚'
+        },
+        {
+          category: '保養品',
+          route: 'cat-care',
+          description: '貓用牙刷牙膏、化毛膏、營養粉'
         }
-        case 'breadcrumb': {
-          const arr = [theClass, theType]
-          const routes = []
-          arr.reduce(function (acc, cur, ind) {
-            if (typeof cur !== 'string') {
-              const concat = acc + `/${cur.eng}`
-              routes.push({
-                path: concat + '/all',
-                name: arr[ind].zhTw
-              })
-              return concat
-            } else {
-              return acc
-            }
-          }, '/products')
-          routes[routes.length - 1].last = true
-          return routes
-        }
-      }
+      ]
     }
   }
 }
