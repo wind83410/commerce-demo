a5d1e747d6a581ae8c67d586566641cd50767e9b
Mack Chen <wind83410@gmail.com>
2020-10-25 22:02:08 +0800

    產品頁內展示類似商品的元件

---
+++ src/components/ProductCardMini.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-link class="product-card-mini d-block" :to="`/products/${categoryRoute}/${prod.id}`">
+    <img class="product-card-mini__img img-fluid d-block mx-auto" :src="prod.imageUrl" alt="">
+    <em class="product-card-mini__price">$ {{ prod.price }}</em>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: ['prod', 'categoryRoute']
+}
+</script>
