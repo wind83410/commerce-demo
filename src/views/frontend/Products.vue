da7fee8f9e1c9ccec63f4567af9b910dd6271195
Mack Chen <wind83410@gmail.com>
2020-11-26 13:32:45 +0800

    先分類商品再產生列表（商品種類、品牌和標籤）

--- src/views/frontend/Products.vue
+++ src/views/frontend/Products.vue
@@ -7,7 +7,7 @@
             <a
               href="#"
               class="list-group-item list-group-item-action"
-              :class="{'active': type === 'all'}"
+              :class="{ active: type === 'all' }"
               @click.prevent="
                 type = 'all'
                 focusBrand = 'all'
@@ -17,7 +17,7 @@
             <a
               href="#"
               class="list-group-item list-group-item-action"
-              :class="{'active': type === subclass}"
+              :class="{ active: type === subclass }"
               v-for="(subclass, ind) in subclassList"
               :key="ind"
               @click.prevent="
@@ -29,7 +29,12 @@
           </nav>
           <nav class="mt-3">
             <label for="brands" class="h4">商品品牌</label>
-            <select name="brands" id="brands" class="form-control" v-model="focusBrand">
+            <select
+              name="brands"
+              id="brands"
+              class="form-control"
+              v-model="focusBrand"
+            >
               <option value="" disabled selected>請選擇品牌</option>
               <option value="all">所有品牌</option>
               <option v-for="brand in brandList" :key="brand" :value="brand">
@@ -39,6 +44,18 @@
           </nav>
           <nav class="mt-3">
             <div class="h4">標籤</div>
+            <div class="btn-group-toggle">
+              <label
+                v-for="(tag, ind) in tagList"
+                :key="ind"
+                class="btn btn-sm btn-outline-primary mx-1"
+                :class="{ active: isChecked(tag) }"
+              >
+                <input type="checkbox" v-model="checkedTags" :value="tag" />
+                <font-awesome-icon icon="tags" />
+                {{ $t(`tags.${tag}`) }}
+              </label>
+            </div>
           </nav>
         </div>
       </div>
@@ -49,7 +66,11 @@
             class="col-lg-4 col-md-6 d-flex"
             :key="prod.id"
           >
-            <ProductCard :prod="prod" :category-route="categoryToRoute(prod.category.class)" class="m-2" />
+            <ProductCard
+              :prod="prod"
+              :category-route="categoryToRoute(prod.category.class)"
+              class="m-2"
+            />
           </div>
         </section>
       </div>
@@ -59,14 +80,14 @@
 
 <script>
 import { signs } from '@/assets/js/mixins'
-import { mapState } from 'vuex'
 import ProductCard from '@/components/ProductCard'
 
 export default {
   data () {
     return {
       focusBrand: 'all',
-      type: 'all'
+      type: 'all',
+      checkedTags: []
     }
   },
   mixins: [signs],
@@ -75,17 +96,37 @@ export default {
       return this.signs.find((el) => el.route === route).category
     },
     categoryToRoute (category) {
-      return this.signs.find(el => el.category === category).route
+      return this.signs.find((el) => el.category === category).route
+    },
+    haveTag (prodTags) {
+      if (!this.checkedTags.length) {
+        return true // omit the check
+      } else if (!prodTags) {
+        return false
+      } else {
+        return (
+          this.checkedTags.findIndex((el) =>
+            prodTags.find((tag) => tag === el)
+          ) !== -1
+        )
+      }
+    },
+    isChecked (tagToCheck) {
+      return this.checkedTags.some((tag) => tag === tagToCheck)
     }
   },
   computed: {
+    enabledProducts () {
+      const className = this.routeToCategory(this.$route.params.category)
+      return this.$store.state.products.filter(
+        (el) => el.category.class === className && parseInt(el.is_enabled)
+      )
+    },
     categorize () {
       const vm = this
-      const className = this.routeToCategory(vm.$route.params.category)
-      return vm.products.filter(function (ele) {
+      return vm.enabledProducts.filter(function (ele) {
         return (
-          parseInt(ele.is_enabled) &&
-          ele.category.class === className &&
+          vm.haveTag(ele.tags) &&
           (vm.type === 'all' ? true : ele.category.type === vm.type) &&
           (vm.focusBrand === 'all' ? true : vm.focusBrand === ele.title.brand)
         )
@@ -94,10 +135,8 @@ export default {
     brandList () {
       const brands = []
       const vm = this
-      const className = this.routeToCategory(vm.$route.params.category)
-      this.products.forEach(function (el) {
+      this.enabledProducts.forEach(function (el) {
         if (
-          className === el.category.class &&
           (vm.type === 'all' ? true : vm.type === el.category.type) &&
           !(brands.length && brands.some((seen) => seen === el.title.brand))
         ) {
@@ -108,10 +147,8 @@ export default {
     },
     subclassList () {
       const subclasses = []
-      const className = this.routeToCategory(this.$route.params.category)
-      this.products.forEach((el) => {
+      this.enabledProducts.forEach((el) => {
         if (
-          className === el.category.class &&
           !(
             subclasses.length &&
             subclasses.some((seen) => seen === el.category.type)
@@ -122,11 +159,28 @@ export default {
       })
       return subclasses
     },
-    ...mapState(['products'])
+    tagList () {
+      const allTags = []
+      this.enabledProducts.forEach((el) => {
+        if (el.tags) {
+          if (!allTags.length) {
+            allTags.push(...el.tags)
+          } else {
+            el.tags.forEach((prodTag) => {
+              if (!allTags.some((seen) => seen === prodTag)) {
+                allTags.push(prodTag)
+              }
+            })
+          }
+        }
+      })
+      return allTags
+    }
   },
   watch: {
     $route () {
       this.focusBrand = this.type = 'all'
+      this.checkedTags = []
     }
   },
   components: {
