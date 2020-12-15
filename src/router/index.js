304960c12dfbbe663652dcbd6f67c38200bc984f
Mack Chen <wind83410@gmail.com>
2020-12-15 12:20:04 +0800

    讓路由名稱和元件名稱一致

--- src/router/index.js
+++ src/router/index.js
@@ -6,52 +6,52 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    component: () => import('../views/frontend/Layout.vue'),
+    component: () => import('@/views/frontend/Layout.vue'),
     children: [
       {
         path: '',
         name: 'Index',
-        component: () => import('../views/frontend/Index.vue')
+        component: () => import('@/views/frontend/Index.vue')
       },
       {
         path: 'products/:category/',
-        component: () => import('../views/frontend/ProductsLayout.vue'),
+        component: () => import('@/views/frontend/ProductsLayout.vue'),
         children: [
           {
             path: '',
             name: 'Products',
-            component: () => import('../views/frontend/Products.vue')
+            component: () => import('@/views/frontend/Products.vue')
           },
           {
             path: ':itemId',
             name: 'Product detail',
-            component: () => import('../views/frontend/ProductDetail.vue')
+            component: () => import('@/views/frontend/ProductDetail.vue')
           }
         ]
       },
       {
         path: 'check',
-        component: () => import('../views/frontend/Register.vue'),
+        component: () => import('@/views/frontend/CheckLayout.vue'),
         children: [
           {
             path: '',
             name: 'Check',
-            component: () => import('../views/frontend/Check.vue')
+            component: () => import('@/views/frontend/Check.vue')
           },
           {
-            path: 'recipient',
-            name: 'Recipient',
-            component: () => import('../views/frontend/Recipient.vue'),
+            path: 'customer',
+            name: 'Check customer',
+            component: () => import('@/views/frontend/CheckCustomer.vue'),
             meta: {
               pathFrom: '/check'
             }
           },
           {
-            path: 'clinch/:orderId',
-            name: 'Deal clinched',
-            component: () => import('../views/frontend/OrderCode.vue'),
+            path: 'order/:orderId',
+            name: 'Check order',
+            component: () => import('@/views/frontend/CheckOrder.vue'),
             meta: {
-              pathFrom: '/check/recipient',
+              pathFrom: '/check/customer',
               orderSentCheck: true
             }
           }
@@ -61,25 +61,25 @@ const routes = [
   },
   {
     path: '/admin',
-    name: 'Backstage',
-    component: () => import('../views/backend/BackendLayout.vue'),
+    name: 'Admin',
+    component: () => import('@/views/backend/AdminLayout.vue'),
     children: [
       {
         path: 'product-list',
-        name: 'Backstage product list',
-        component: () => import('../views/backend/ProductsList.vue'),
+        name: 'Admin product list',
+        component: () => import('@/views/backend/AdminProductsList.vue'),
         meta: { requireAuth: true }
       },
       {
         path: 'coupons',
-        name: 'Backstage coupon list',
-        component: () => import('@/views/backend/CouponList.vue'),
+        name: 'Admin coupon list',
+        component: () => import('@/views/backend/AdminCouponList.vue'),
         meta: { requireAuth: true }
       },
       {
         path: 'orders',
         name: 'Backstage order list',
-        component: () => import('@/views/backend/OrderList.vue'),
+        component: () => import('@/views/backend/AdminOrderList.vue'),
         meta: { requireAuth: true }
       }
     ]
