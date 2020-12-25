b13ac3a86894ca454dd2178c51789ede8530b659
Mack Chen <wind83410@gmail.com>
2020-12-25 22:02:52 +0800

    ProductLayout.vue: 行動版內去除 banner 上留白 (margin-top)，並調暗背景混合色突出標題

--- src/views/frontend/ProductsLayout.vue
+++ src/views/frontend/ProductsLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-nav">
-    <header class="container py-3 my-3 p-banner d-flex flex-column justify-content-center align-items-start" :class="bannerBg">
+    <header class="container py-3 mb-3 my-sm-3 p-banner d-flex flex-column justify-content-center align-items-start" :class="bannerBg">
       <div class="p-banner__text-wrap p-3">
         <div class="p-banner__category">{{ sign.category }}</div>
         <div class="p-banner__description">{{ sign.description }}</div>
